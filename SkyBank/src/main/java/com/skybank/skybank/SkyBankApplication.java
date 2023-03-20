f3625591e20373292e1e3362e39e58567d1096df
pxpe <pepefx.lr@uma.es>
2023-03-20 11:28:20 +0100

    Spring Project

---
+++ SkyBank/src/main/java/com/skybank/skybank/SkyBankApplication.java
@@ -0,0 +1,13 @@
+package com.skybank.skybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyBankApplication.class, args);
+	}
+
+}
