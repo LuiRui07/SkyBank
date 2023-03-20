c46d19a40943ac09d374c3bca637e8ed0bcad03d
Luis Ruiz <100932068+LuiRui07@users.noreply.github.com>
2023-03-20 20:21:47 +0100

    Creación correcta del proyecto

---
+++ skybank/src/main/java/com/example/skybank/SkybankApplication.java
@@ -0,0 +1,13 @@
+package com.example.skybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkybankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkybankApplication.class, args);
+    }
+
+}
---
+++ skybank/src/test/java/com/example/skybank/SkybankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkybankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
