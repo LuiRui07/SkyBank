86a0c4ffc9a120a65bb8be2114bd16d3f5709679
Luis Ruiz <100932068+LuiRui07@users.noreply.github.com>
2023-04-24 13:49:01 +0200

    inicio

--- skybank/src/main/java/com/example/skybank/dao/CustomerRepository.java
+++ skybank/src/main/java/com/example/skybank/dao/CustomerRepository.java
@@ -1,9 +1,14 @@
 package com.example.skybank.dao;
 
 import com.example.skybank.entity.ClienteEntity;
+import com.example.skybank.entity.EmpresaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 public interface CustomerRepository extends JpaRepository<ClienteEntity, Integer> {
 
+    @Query("select c from ClienteEntity c where c.nombre= :user and c.password = :password")
+    ClienteEntity autenticar(@Param("user") String name, @Param("password") String contra);
 }
