ae26c68b5652f87bbeb0ed92ce1ac39ffda5a321
Luis Ruiz <100932068+LuiRui07@users.noreply.github.com>
2023-05-04 13:40:25 +0200

    Update skybankdb.sql

--- skybankdb.sql
+++ skybankdb.sql
@@ -65,7 +65,7 @@ CREATE TABLE `autorizado` (
   `solicituddesbloqueo` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `fk_Socio_Empresa1_idx` (`idempresa`) /*!80000 INVISIBLE */,
-  CONSTRAINT `fk_Socio_Empresa10` FOREIGN KEY (`idempresa`) REFERENCES `empresa` (`idempresa`) ON UPDATE CASCADE
+  CONSTRAINT `fk_Socio_Empresa10` FOREIGN KEY (`idempresa`) REFERENCES `empresa` (`idempresa`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -101,8 +101,7 @@ CREATE TABLE `cliente` (
   `planta` int DEFAULT NULL,
   `region` varchar(100) DEFAULT NULL,
   `cp` int NOT NULL,
-  `solicitud` int NOT NULL DEFAULT '0',
-  `aceptado` int NOT NULL DEFAULT '0',
+  `verificado` int NOT NULL DEFAULT '0',
   `bloqueado` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`idcliente`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
@@ -114,7 +113,7 @@ CREATE TABLE `cliente` (
 
 LOCK TABLES `cliente` WRITE;
 /*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
-INSERT INTO `cliente` VALUES (1,'11223344J','Luis','Ruiz','Nuñez','2001-10-18','Marcos Zapata','lui@gmail','lui',2,'Mala','España',1,'Andaluz',29017,0,0,0),(2,'11445544K','Manuel','Rodriguez','Meh','2002-10-31','Santo Domingo','manu@gmail','manu',2,'Teruel','España',1,'Aragon',11111,0,0,0);
+INSERT INTO `cliente` VALUES (1,'11223344J','Luis','Ruiz','Nuñez','2001-10-18','Marcos Zapata','lui@gmail','lui',2,'Mala','España',1,'Andaluz',29017,1,0),(2,'11445544K','Manuel','Rodriguez','Meh','2002-10-31','Santo Domingo','manu@gmail','manu',2,'Teruel','España',1,'Aragon',11111,0,0);
 /*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -168,7 +167,7 @@ CREATE TABLE `cuenta` (
   KEY `Divisa_idx` (`divisa`),
   CONSTRAINT `Divisa` FOREIGN KEY (`divisa`) REFERENCES `divisa` (`iddivisa`),
   CONSTRAINT `fk_Cuenta_Cliente1` FOREIGN KEY (`idcliente`) REFERENCES `cliente` (`idcliente`),
-  CONSTRAINT `fk_Cuenta_Empresa1` FOREIGN KEY (`idempresa`) REFERENCES `empresa` (`idEmpresa`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `fk_Cuenta_Empresa1` FOREIGN KEY (`idempresa`) REFERENCES `empresa` (`idempresa`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -369,7 +368,7 @@ CREATE TABLE `socio` (
   `solicituddesbloqueo` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `idEmpresa_idx` (`idempresa`),
-  CONSTRAINT `idEmpresa` FOREIGN KEY (`idempresa`) REFERENCES `empresa` (`idempresa`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `idEmpresa` FOREIGN KEY (`idempresa`) REFERENCES `empresa` (`idempresa`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -381,6 +380,29 @@ LOCK TABLES `socio` WRITE;
 /*!40000 ALTER TABLE `socio` DISABLE KEYS */;
 INSERT INTO `socio` VALUES (1,2,'1324324','paco','merte',NULL,'2000-03-12',0,'paco@danone.es','paco','poopo',0,0,'Malaka','Spain',NULL,0,0),(2,9,'12345X','paquito','paco','','4223-03-12',0,'paco@paco.com','paco','poopo',0,0,'Malaka','spain','',0,0);
 /*!40000 ALTER TABLE `socio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tipo-operacion`
+--
+
+DROP TABLE IF EXISTS `tipo-operacion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tipo-operacion` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '1. Transferencia',
+  `Tipo` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tipo-operacion`
+--
+
+LOCK TABLES `tipo-operacion` WRITE;
+/*!40000 ALTER TABLE `tipo-operacion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tipo-operacion` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -416,4 +438,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-04 12:55:34
+-- Dump completed on 2023-05-04 13:34:23
