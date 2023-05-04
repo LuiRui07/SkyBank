a696b1bb53b193faf615a8641f7a0b5807d69aae
Luis Ruiz <100932068+LuiRui07@users.noreply.github.com>
2023-05-04 16:55:15 +0200

    merge Gestor

--- skybank/src/main/java/com/example/skybank/dao/EmpresaRepository.java
+++ skybank/src/main/java/com/example/skybank/dao/EmpresaRepository.java
@@ -10,9 +10,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmpresaRepository extends JpaRepository<EmpresaEntity,Integer> {
 
     @Query("select distinct e from EmpresaEntity e where e.nombre = :user and e.passwordempresa = :password")
     EmpresaEntity autenticar(@Param("user") String name,@Param("password") String contra);
+
+    @Query("select e from EmpresaEntity e where e.verificado = 0")
+    List<EmpresaEntity> getPendientesVerificar();
 }
